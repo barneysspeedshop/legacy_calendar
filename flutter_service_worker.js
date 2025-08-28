778e9ddff179e3b4c0f95ca2db96d3c554334153
barneysspeedshop <barneysspeedshop@users.noreply.github.com>
2025-08-28 02:12:04 +0000

    deploy: 67d6ae914ac530ecd5a87e82e11c267c14aa2c36

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -33,7 +33,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/packages/font_awesome_flutter/lib/fonts/Font%2520Awesome%25207%2520Brands-Regular-400.otf": "440da663f17184f21f007a3a2bf60f69",
 "assets/packages/font_awesome_flutter/lib/fonts/Font%2520Awesome%25207%2520Free-Solid-900.otf": "7d6aba1f2f98c2fe6025a283b36216a4",
 "assets/packages/font_awesome_flutter/lib/fonts/Font%2520Awesome%25207%2520Free-Regular-400.otf": "75252f97dccbc1fcef0ee3da7dee86d7",
-"flutter_bootstrap.js": "8daaab452d438eebafce4a1ee3ae0856",
+"flutter_bootstrap.js": "8de56d5615d0b5c8b1130a563874cd1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "d1ae74a6aab64b39aef90a1cd68236d8",
 "/": "d1ae74a6aab64b39aef90a1cd68236d8"};
